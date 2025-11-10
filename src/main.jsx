e1accfcc13bca80f3bc6e9817eea5ea9b213026a
Nico? <84991759+NiccoDorn@users.noreply.github.com>
2025-11-10 05:46:52 +0100

    removed copied over license notice

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,3 @@
-// Copyright (c) 2025 Jane Doe
-// Licensed under the MIT License. See LICENSE file in the project root for details.
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
